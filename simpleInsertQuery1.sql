39af1a0f4f6b77234c091a9e7bf3a9b6956faeaf
Ingrid Lootsma <ingrid.lootsma@gmail.com>
2020-09-05 20:53:43 +0200

    Add an insert query after dbimport ran

---
+++ simpleInsertQuery1.sql
@@ -0,0 +1,14 @@
+﻿select top 10 
+'insert into corporateknowhowalert ([UniqueId], [ItemId], [Title], [Content], [Description], [Data], [AuthorEmail], [ImageFileName], [TagValue], [CreatedOn], [ModifiedOn]) VALUES (' +
+'''' + convert(varchar(max), cka.uniqueid) + '''' + ',' +
+convert(varchar(10), cka.ItemId) + ',' +
+'''' + replace(convert(varchar(max), cka.title), '''', '''''') + '''' + ',' +
+'''' + replace(convert(varchar(max), ISNULL(cka.content, '')), '''', '''''') + '''' + ',' +
+'''' + replace(convert(varchar(max), ISNULL(cka.[description], '')), '''', '''''') + '''' + ',' +
+'''' + replace(convert(varchar(max), ISNULL(cka.[data], '')), '''', '''''') + '''' + ',' +
+'''' + convert(varchar(max), cka.AuthorEmail) + '''' + ',' +
+'''' + convert(varchar(max), ISNULL(cka.ImageFileName, '')) + '''' + ',' +
+'''' + replace(convert(varchar(max), cka.TagValue), '''', '''''') + '''' + ',' +
+'''' + convert(varchar(max), cka.CreatedOn) + '''' + ',' +
+'''' + convert(varchar(max), cka.ModifiedOn) + '''' + ')'
+from corporateknowhowalert cka
